bdee5544f7da77506520fe52f5e49f22354ab945
aquinovgb <vbaquino@up.edu.ph>
2025-11-07 01:31:54 +0800

    fix: Added missing routers for userController

--- shared-space-api/routes/userRouter.js
+++ shared-space-api/routes/userRouter.js
@@ -1,6 +1,10 @@
 import express from 'express';
-import { registerUser, loginUser, getRegisteredUsers } from '../controllers/userController.js'; 
-import { verifyToken } from '../middleware/auth.js'; 
+import { registerUser, loginUser, getRegisteredUsers, 
+    sendFriendRequest, acceptFriendRequest, declineFriendRequest, 
+    removeFriend, getFriendsList, getPendingRequests,
+    findByUserEmail, deleteUser, updateUser, findCurrentUser 
+} from '../controllers/userController.js'; 
+import { isAdmin, verifyToken } from '../middleware/auth.js'; 
 
 const router = express.Router();
 
@@ -8,5 +12,17 @@ router.post('/register', registerUser);
 router.post('/login', loginUser);
 
 router.get('/all', verifyToken, getRegisteredUsers);
+
+router.get('/me', verifyToken, findCurrentUser);
+router.put('/update', verifyToken, updateUser);
+router.post('/find', verifyToken, findByUserEmail);
+router.delete('/delete', verifyToken, isAdmin, deleteUser);
+
+router.post('/friends/request', verifyToken, sendFriendRequest);
+router.post('/friends/accept', verifyToken, acceptFriendRequest);
+router.post('/friends/decline', verifyToken, declineFriendRequest);
+router.delete('/friends/remove', verifyToken, removeFriend);
+router.get('/friends', verifyToken, getFriendsList);
+router.get('/friends/pending', verifyToken, getPendingRequests);
 
 export default router;
